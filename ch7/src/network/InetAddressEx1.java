7229300cf1e338a9c365754f5709805c719561c5
seungoh <seungoh6@gmail.com>
2023-03-16 17:43:08 +0900

    InetAddressEx1 업데이트

--- ch7/src/network/InetAddressEx1.java
+++ ch7/src/network/InetAddressEx1.java
@@ -20,6 +20,7 @@ public class InetAddressEx1 {
 	public static void main(String[] args) {
 		
 		//네트워크 관련 
+		//네트워크 관련
 		try {
 			InetAddress address = InetAddress.getByName("naver.com");
 			System.out.println("Host Name: " + address.getHostName());
